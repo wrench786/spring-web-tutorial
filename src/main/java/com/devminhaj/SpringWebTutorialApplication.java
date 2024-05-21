83539b465338750e2980c95b14dc91a47113aac2
Md. Minhajul Islam <wrench786@gmail.com>
2024-05-21 15:28:25 +0600

    Created Spring Web Project

---
+++ src/main/java/com/devminhaj/SpringWebTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.devminhaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devminhaj/SpringWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devminhaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
